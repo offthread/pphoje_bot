b1a039eeaeeac4815e59d69a9e5633400003e604
Diego Maia <diegocmaia@gmail.com>
2017-05-15 23:28:54 -0300

    Adding body-parser

--- bot/messenger/server.js
+++ bot/messenger/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import _ from 'lodash'
 
 import router from './routes'
 
 const app = express()
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3001
 
