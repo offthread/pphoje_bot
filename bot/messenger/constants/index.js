690847aab9c41d0a790a0fb8d925bdd79e6a8d2b
Diego Maia <diegocmaia@gmail.com>
2017-05-19 17:40:15 -0300

    New commands

--- bot/messenger/constants/index.js
+++ bot/messenger/constants/index.js
@@ -1,5 +1,7 @@
 export default {
   NUMBER_REGULAR_EXPRESSION: /^-?[0-9]+$/,
 	DATE_REGULAR_EXPRESSION: /^[0-3]?[0-9]\/[0-3]?[0-9]$/,
-  WEEK_DAYS: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB']
+  WEEK_DAYS: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'],
+  TODAY_TEXT: 'HOJE',
+  TOMORROW_TEXT: 'AMANHA'
 }
