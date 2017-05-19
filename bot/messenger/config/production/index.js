149e2a3378506967506c347904558d1abcd7b4a0
Diego Maia <diegocmaia@gmail.com>
2017-05-19 17:10:06 -0300

    Help message

--- bot/messenger/config/production/index.js
+++ bot/messenger/config/production/index.js
@@ -7,9 +7,9 @@ export default {
     Você pode consultar a programação das seguintes formas:
 
     1 - Digitando a data desejada no formato DD/MM ou DD. Exemplo: 24/06 ou 15;
-    2 - Digitando a sigla do dia desejado dos próximos 7 dias: DOM, SEG, TER, QUA, QUI, SEX ou SAB.
+    2 - Digitando a sigla do dia desejado (dos próximos 7 dias): DOM, SEG, TER, QUA, QUI, SEX ou SAB.
 
-    Dúvidas ou sugestões? Entre em contato com os desenvolvedores utilizando o e-mail offthread@gmail.com
+    Dúvidas ou sugestões? Envie e-mail para offthread@gmail.com
     Desenvolvido por OFF Thread
   `,
   page_access_token: 'EAASLgFOkSxYBAE8gzcR28BDYV3TybZBeqYJsJcdnruRie0pRmCN1nS0CghRJ8AqpiEkNGzE3kjC4IoxfPUUDZCw3EWz5RpBMreYJyZCZCpRPhjnLVTkiyYuAAQ40J0ux9lAs5RzZBdXsydZCQDpM4sVbbTFkRMx4Sg78uZBnZCTmlAZDZD'
