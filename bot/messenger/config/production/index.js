5f3ee7db67b13a65ccb640fc5dea7a518437120b
Diego Maia <diegocmaia@gmail.com>
2017-05-19 17:43:40 -0300

    Help text

--- bot/messenger/config/production/index.js
+++ bot/messenger/config/production/index.js
@@ -9,6 +9,10 @@ export default {
     #1 - Digitando a data desejada no formato DD/MM ou DD. Exemplo: 24/06 ou 15;
     #2 - Digitando a sigla do dia desejado (dos próximos 7 dias): DOM, SEG, TER, QUA, QUI, SEX ou SAB.
 
+    Mas calma! Não quer pensar na data? Envia 'Hoje' ou 'Amanhã' que eu quebro essa! :)
+
+    Lembrando que o Maior e Melhor São João do Mundo vai de 02 de junho a 02 de julho de 2017. APROVEITE!
+
     Dúvidas ou sugestões? Envie e-mail para offthread@gmail.com.
 
     Desenvolvido por OFF Thread.
