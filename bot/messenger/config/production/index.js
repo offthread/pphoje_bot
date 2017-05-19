3afb4ad92d73d4c617355bd3e8297548b95e78b7
Diego Maia <diegocmaia@gmail.com>
2017-05-19 17:14:37 -0300

    Help message - Ano

--- bot/messenger/config/production/index.js
+++ bot/messenger/config/production/index.js
@@ -4,13 +4,14 @@ export default {
   sj_end_day: '2017-07-03',
   help_command: 'AJUDA',
   help_text: `
-    Você pode consultar a programação das seguintes formas:
+    Você pode consultar a programação do São João de Campina Grande 2017 das seguintes formas:
 
-    1 - Digitando a data desejada no formato DD/MM ou DD. Exemplo: 24/06 ou 15;
-    2 - Digitando a sigla do dia desejado (dos próximos 7 dias): DOM, SEG, TER, QUA, QUI, SEX ou SAB.
+    #1 - Digitando a data desejada no formato DD/MM ou DD. Exemplo: 24/06 ou 15;
+    #2 - Digitando a sigla do dia desejado (dos próximos 7 dias): DOM, SEG, TER, QUA, QUI, SEX ou SAB.
 
-    Dúvidas ou sugestões? Envie e-mail para offthread@gmail.com
-    Desenvolvido por OFF Thread
+    Dúvidas ou sugestões? Envie e-mail para offthread@gmail.com.
+
+    Desenvolvido por OFF Thread.
   `,
   page_access_token: 'EAASLgFOkSxYBAE8gzcR28BDYV3TybZBeqYJsJcdnruRie0pRmCN1nS0CghRJ8AqpiEkNGzE3kjC4IoxfPUUDZCw3EWz5RpBMreYJyZCZCpRPhjnLVTkiyYuAAQ40J0ux9lAs5RzZBdXsydZCQDpM4sVbbTFkRMx4Sg78uZBnZCTmlAZDZD'
 }
